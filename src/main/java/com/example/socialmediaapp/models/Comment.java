3df28f13a0191a6dcbcfe4073bcaad32082aa679
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-06-28 08:50:19 -0300

    Initial commit

---
+++ src/main/java/com/example/socialmediaapp/models/Comment.java
@@ -0,0 +1,34 @@
+package com.example.socialmediaapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@Transactional
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    private String text;
+
+}
---
+++ src/main/java/com/example/socialmediaapp/models/Post.java
@@ -0,0 +1,36 @@
+package com.example.socialmediaapp.models;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@Transactional
+public class Post {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String caption;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+    
+}
