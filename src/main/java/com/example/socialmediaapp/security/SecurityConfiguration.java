689c3fd9bcd6e2c4c0772e613c18fc46d93439e0
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-07-04 15:41:48 -0300

    Added missing imports

--- src/main/java/com/example/socialmediaapp/security/SecurityConfiguration.java
+++ src/main/java/com/example/socialmediaapp/security/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package com.example.socialmediaapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
